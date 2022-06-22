ff13f3e545d7b8eed677ee322ecefe1408d7d0e5
Jordan Radushev <dani.radushev@gmail.com>
2022-06-22 16:52:28 +0300

    exercise: fast fetch done

--- cmd/fast_fetch/internal/server/server.go
+++ cmd/fast_fetch/internal/server/server.go
@@ -1,5 +1,11 @@
 package server
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 func FetchUsers() []string {
 	return nil
 }
@@ -7,32 +13,116 @@ func FetchUsers() []string {
 type Server struct {
 }
 
-type ServerResponse struct {
-	Users           []string
-	Payments        []string
+type Entity struct {
+	User            string
+	Payment         string
 	DeliveryTracker string
 }
+type ServerResponse struct {
+	TotalResults int
+	Resources    []*Entity
+}
+
+// WaitGroup is used to wait for the program to finish goroutines.
+var wg sync.WaitGroup
 
 func NewServer() *Server {
 	return &Server{}
 }
 
 func (s *Server) fetchUsers() []string {
-	return nil
+	defer wg.Done()
+
+	result := []string{"Jordan", "John", "Alex"}
+	// Simulate short delay
+	time.Sleep(3 * time.Second)
+	return result
 }
 
 func (s *Server) fetchPayments() []string {
-	return nil
+	defer wg.Done()
+
+	result := []string{"$10.99", "$19.99", "$25.89"}
+	// Simulate short delay
+	time.Sleep(3 * time.Second)
+	return result
 }
 
 func (s *Server) fetchDeliveryTracker() []string {
-	return nil
+	defer wg.Done()
+
+	result := []string{"completed", "pending", "pending"}
+	// Simulate short delay
+	time.Sleep(3 * time.Second)
+	return result
 }
 
+func toResource(user string, payment string, deliveryTracker string) *Entity {
+	return &Entity{
+		User:            user,
+		Payment:         payment,
+		DeliveryTracker: deliveryTracker,
+	}
+}
 func (s *Server) FetchAllSync() (*ServerResponse, error) {
-	return nil, nil
+	startTime := time.Now()
+	fmt.Printf("Start time: %v \n", startTime.Format("15:04:05"))
+	resources := make([]*Entity, 0, 3)
+
+	users := s.fetchUsers()
+	payments := s.fetchPayments()
+	deliveries := s.fetchDeliveryTracker()
+
+	for i := 0; i < 3; i++ {
+		resources = append(resources, toResource(users[i], payments[i], deliveries[i]))
+	}
+
+	response := &ServerResponse{
+		TotalResults: 3,
+		Resources:    resources,
+	}
+	endTime := time.Now()
+
+	fmt.Printf("End time: %v \n", endTime.Format("15:04:05"))
+	fmt.Printf("Time Elapsed: ~%v seconds \n", (endTime.UnixMilli()-startTime.UnixMilli())/1000)
+
+	return response, nil
 }
 
 func (s *Server) FetchAll() (*ServerResponse, error) {
-	return nil, nil
+	startTime := time.Now()
+	fmt.Printf("Start time: %v \n", startTime.Format("15:04:05"))
+	resources := make([]*Entity, 0, 3)
+
+	wg.Add(3)
+
+	var users, payments, deliveries []string
+
+	go func() {
+		users = s.fetchUsers()
+	}()
+	go func() {
+		payments = s.fetchPayments()
+	}()
+
+	go func() {
+		deliveries = s.fetchDeliveryTracker()
+	}()
+
+	wg.Wait()
+
+	for i := 0; i < 3; i++ {
+		resources = append(resources, toResource(users[i], payments[i], deliveries[i]))
+	}
+
+	response := &ServerResponse{
+		TotalResults: 3,
+		Resources:    resources,
+	}
+	endTime := time.Now()
+
+	fmt.Printf("End time: %v \n", endTime.Format("15:04:05"))
+	fmt.Printf("Time Elapsed: ~%v seconds \n", (endTime.UnixMilli()-startTime.UnixMilli())/1000)
+
+	return response, nil
 }
--- cmd/fast_fetch/main.go
+++ cmd/fast_fetch/main.go
@@ -6,15 +6,8 @@ import (
 	"github.com/JordanRad/concurrency-practice/cmd/fast_fetch/internal/server"
 )
 
-type Operations interface {
-	FetchUsers() []string
-	FetchPayments() []string
-	FetchDeliveryTracker() []string
-}
-
 func main() {
 	serverHandler := server.NewServer()
-	resp, err := serverHandler.FetchAllSync()
-	fmt.Println(resp, err)
-	fmt.Println("Hello from Fast Fetcher")
+	fmt.Println("Fetching asynchronously....")
+	_, _ = serverHandler.FetchAll()
 }
